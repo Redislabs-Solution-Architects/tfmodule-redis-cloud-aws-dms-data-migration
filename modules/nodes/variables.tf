ccfb9a58b1d9166f5dad19cf80205bdfe060ee4c
brandonamos <bpamos21@gmail.com>
2023-06-16 14:24:28 -0600

    updated outputs

--- modules/nodes/variables.tf
+++ modules/nodes/variables.tf
@@ -105,168 +105,7 @@ variable "internal-rules" {
       to_port   = "3306"
       protocol  = "tcp"
       comment   = "MySQL from VPC"
-    },
-    # {
-    #   type      = "ingress"
-    #   from_port = "1968"
-    #   to_port   = "1968"
-    #   protocol  = "tcp"
-    #   comment   = "Proxy traffic (Internal use)"
-    # },
-    # {
-    #   type      = "ingress"
-    #   from_port = "3333"
-    #   to_port   = "3341"
-    #   protocol  = "tcp"
-    #   comment   = "Cluster traffic (Internal use)"
-    # },
-    # {
-    #   type      = "ingress"
-    #   from_port = "3343"
-    #   to_port   = "3344"
-    #   protocol  = "tcp"
-    #   comment   = "Cluster traffic (Internal use)"
-    # },
-    # {
-    #   type      = "ingress"
-    #   from_port = "36379"
-    #   to_port   = "36380"
-    #   protocol  = "tcp"
-    #   comment   = "Cluster traffic (Internal use)"
-    # },
-    # {
-    #   type      = "ingress"
-    #   from_port = "8001"
-    #   to_port   = "8001"
-    #   protocol  = "tcp"
-    #   comment   = "Traffic from application to RS Discovery Service"
-    # },
-    # {
-    #   type      = "ingress"
-    #   from_port = "8002"
-    #   to_port   = "8002"
-    #   protocol  = "tcp"
-    #   comment   = "System health monitoring"
-    # },
-    # {
-    #   type      = "ingress"
-    #   from_port = "8004"
-    #   to_port   = "8004"
-    #   protocol  = "tcp"
-    #   comment   = "System health monitoring"
-    # },
-    # {
-    #   type      = "ingress"
-    #   from_port = "8006"
-    #   to_port   = "8006"
-    #   protocol  = "tcp"
-    #   comment   = "System health monitoring"
-    # },
-    # {
-    #   type      = "ingress"
-    #   from_port = "8443"
-    #   to_port   = "8443"
-    #   protocol  = "tcp"
-    #   comment   = "Secure (HTTPS) access to the management web UI"
-    # },
-    # {
-    #   type      = "ingress"
-    #   from_port = "8444"
-    #   to_port   = "8444"
-    #   protocol  = "tcp"
-    #   comment   = "nginx <-> cnm_http/cm traffic (Internal use)"
-    # },
-    # {
-    #   type      = "ingress"
-    #   from_port = "9080"
-    #   to_port   = "9080"
-    #   protocol  = "tcp"
-    #   comment   = "nginx <-> cnm_http/cm traffic (Internal use)"
-    # },
-    # {
-    #   type      = "ingress"
-    #   from_port = "9081"
-    #   to_port   = "9081"
-    #   protocol  = "tcp"
-    #   comment   = "For CRDB management (Internal use)"
-    # },
-    # {
-    #   type      = "ingress"
-    #   from_port = "8070"
-    #   to_port   = "8071"
-    #   protocol  = "tcp"
-    #   comment   = "Prometheus metrics exporter"
-    # },
-    # {
-    #   type      = "ingress"
-    #   from_port = "9443"
-    #   to_port   = "9443"
-    #   protocol  = "tcp"
-    #   comment   = "REST API traffic, including cluster management and node bootstrap"
-    # },
-    # {
-    #   type      = "ingress"
-    #   from_port = "10000"
-    #   to_port   = "19999"
-    #   protocol  = "tcp"
-    #   comment   = "Database traffic - if manually creating db ports pare down"
-    # },
-    # {
-    #   type      = "ingress"
-    #   from_port = "20000"
-    #   to_port   = "29999"
-    #   protocol  = "tcp"
-    #   comment   = "Database shards traffic - if manually creating db ports pare down"
-    # },
-    # {
-    #   type      = "ingress"
-    #   from_port = "53"
-    #   to_port   = "53"
-    #   protocol  = "udp"
-    #   comment   = "DNS Traffic"
-    # },
-    # {
-    #   type      = "ingress"
-    #   from_port = "5353"
-    #   to_port   = "5353"
-    #   protocol  = "udp"
-    #   comment   = "DNS Traffic"
-    # },
-    # {
-    #   type      = "ingress"
-    #   from_port = "-1"
-    #   to_port   = "-1"
-    #   protocol  = "icmp"
-    #   comment   = "Ping for connectivity checks between nodes"
-    # },
-    # {
-    #   type      = "egress"
-    #   from_port = "-1"
-    #   to_port   = "-1"
-    #   protocol  = "icmp"
-    #   comment   = "Ping for connectivity checks between nodes"
-    # },
-    # {
-    #   type      = "egress"
-    #   from_port = "0"
-    #   to_port   = "65535"
-    #   protocol  = "tcp"
-    #   comment   = "Let TCP out to the VPC"
-    # },
-    # {
-    #   type      = "egress"
-    #   from_port = "0"
-    #   to_port   = "65535"
-    #   protocol  = "udp"
-    #   comment   = "Let UDP out to the VPC"
-    # },
-    # #    {
-    # #      type      = "ingress"
-    # #      from_port = "8080"
-    # #      to_port   = "8080"
-    # #      protocol  = "tcp"
-    # #      comment   = "Allow for host check between nodes"
-    # #    },
+    }
   ]
 }
 
@@ -274,26 +113,12 @@ variable "external-rules" {
   description = "Security rules to allow for connectivity external to the VPC"
   type        = list(any)
   default = [
-    # {
-    #   type      = "ingress"
-    #   from_port = "53"
-    #   to_port   = "53"
-    #   protocol  = "udp"
-    #   cidr      = ["0.0.0.0/0"]
-    # },
     {
       type      = "egress"
       from_port = "0"
       to_port   = "65535"
       protocol  = "tcp"
       cidr      = ["0.0.0.0/0"]
-    },
-    # {
-    #   type      = "egress"
-    #   from_port = "0"
-    #   to_port   = "65535"
-    #   protocol  = "udp"
-    #   cidr      = ["0.0.0.0/0"]
-    # }
+    }
   ]
 }
